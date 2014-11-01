e803c8fd34172eb45cbdbf6057c5acd135766269
刘兴 <liuxing87327@163.com>
2014-11-01 20:34:49 +0800

    NJBlog
    NJBlog种子框架

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: c-sailor.zhang
+ * Date: 1/23/13
+ * Time: 2:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+module.exports = {
+    cookieSecret: 'njblog',
+    connectionstring: 'mongodb://URrRt070VEvNA1ejIzNennVC:mWFaNXeU097nCPZLwG6V1yjPOI592oXn@mongo.duapp.com:8908/zQVqlLrEWYIrbQyuEjID'
+//    connectionstring: 'mongodb://127.0.0.1:27017/NJBlog'
+    // connectionstring:'mongodb://用户名:密码@IP地址:端口/库名'
+    // connectionstring:'mongodb://localhost/njblog'
+};
